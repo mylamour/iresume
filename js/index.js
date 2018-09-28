2046c2bb218a32f8c33306bec885e42cecd5ce06
mylamour <mylamour@163.com>
2018-09-28 21:07:10 +0800

    seperate resume date to signal file

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ var shell = $('.shell').resizable({
 
 var cmdlist = ['ls', 'less', 'cd', 'cat', 'help', 'whoami', 'resume', 'echo', 'exit']
 var dircontent = ['readme.md', 'blog']
-var readme = "Hi, guys, welcome to my little secret place. i make it use juqery.terminal. and this is static website, so this file readme.md is not exist actually. it only few line string here. As for me, i am learning programing since 5 years ago, when i was go to college. I always have a greate passions to new things. i try to play hacking, programing, reading. and others, also i am writing it in my blog, gitbook. and all above this, it;s just for fun. Hope you would found your interesting things. And have a nice day"
 
 var term = $('.content').terminal({
     help: function () {
@@ -85,16 +84,7 @@ function show() {
     term.push(function (command) {
         document.getElementById("greeting").style.display = "None";
         if (command.match(/^y$/i)) {
-            profiles = {
-                "name": "mour",
-                "sex": "M",
-                "mail": "mour at iami.xyz",
-                "progaming": "Python, Shell, javascript, node, little rust...",
-                "skills": " Web security, Machine Learning, Deep Learning",
-                "status": "Working on BTCC as Security Architect",
-                "exprience": "2 years",
-                "About": "I am a security researcher, also a devops, and with great passion in machine learning and deep learning.",
-            }
+            
             var whoami = Object.keys(profiles).map(function (key) {
                 var value = profiles[key];
                 return '[[b;#fff;]' + key + ']: ' + '[[;springgreen;]' + value + ']';
---
+++ js/resume.js
@@ -0,0 +1,12 @@
+var profiles = {
+    "name": "mour",
+    "sex": "M",
+    "mail": "mour at iami.xyz",
+    "progaming": "Python, Shell, javascript, node, little rust...",
+    "skills": " Web security, Machine Learning, Deep Learning",
+    "status": "Working on BTCC as Security Architect",
+    "exprience": "2 years",
+    "About": "I am a security researcher, also a devops, and with great passion in machine learning and deep learning.",
+}
+
+var readme = "Hi, guys, welcome to my little secret place. i make it use juqery.terminal. and this is static website, so this file readme.md is not exist actually. it only few line string here. As for me, i am learning programing since 5 years ago, when i was go to college. I always have a greate passions to new things. i try to play hacking, programing, reading. and others, also i am writing it in my blog, gitbook. and all above this, it;s just for fun. Hope you would found your interesting things. And have a nice day"
