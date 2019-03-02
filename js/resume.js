789ee9d3a419b1f9a607384cb0f2f7c3b200bbb0
mour <mylamour@163.com>
2019-03-02 17:06:58 +0800

    update resume

--- js/resume.js
+++ js/resume.js
@@ -2,11 +2,11 @@ var profiles = {
     "name": "mour",
     "sex": "M",
     "mail": "mour at iami.xyz",
-    "progaming": "Python, Shell, javascript, node, little rust...",
-    "skills": " Web security, Machine Learning, Deep Learning",
-    "status": "Working on BTCC as Security Architect",
-    "exprience": "2 years",
-    "About": "I am a security researcher, also a devops, and with great passion in machine learning and deep learning.",
+    "skills": "Security Architecture, SDL, Web Security, Progaming, Machine Learning, Deep Learning",
+    "progaming": "Python, Shell, javascript, node, golang ...",
+    "status": "Working on ele.me as Senior Information Security Engineer, Design and implement Solutions",
+    "exprience": "working ≈ 3 years",
+    "About": "I am a security expert、devops, and with great passion in machine learning and deep learning. Always keep learning and Shendu. Since BTCC was closed, I find new job at ele.me which the Take-out service is one of the biggest in China. In that time, it's already belong to Alibaba. So it's opportunity to me show myself on this platform. Actually, I did it. Design and implement the solutions of fundamental security, also in operational analytics. but i found something was strange, more and more weird. No matter what, i learned somthing from here, now i want to help more company is secure",
 }
 
-var readme = "Hi, guys, welcome to my little secret place. i make it use juqery.terminal. and this is static website, so this file readme.md is not exist actually. it only few line string here. As for me, i am learning programing since 5 years ago, when i was go to college. I always have a greate passions to new things. i try to play hacking, programing, reading. and others, also i am writing it in my blog, gitbook. and all above this, it;s just for fun. Hope you would found your interesting things. And have a nice day"
+var readme = "Hi, guys, welcome to my little secret place. i make it use juqery.terminal. and this is static website, so this file readme.md is not exist actually. it only few line string here. As for me, i am learning programing since 5 years ago, when i was go to college. I always have a greate passions to new things. i try to play hacking, programing, reading. and others, also i am writing it in my blog, gitbook. and all above this, it's just for fun. Hope you would found your interesting things. And have a nice day"
